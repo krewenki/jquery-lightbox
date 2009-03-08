232686ad166d2b6f77876d785f0daa0f7e1dcc7e
Scott Windsor <swindsor@gmail.com>
2009-03-08 08:26:21 +0800

    Adding option for disabling the navbar links
    Navbar links can now be enabled/disabled by using the disableNavbarLinks
    option (which defaults to false).
    
    Signed-off-by: Warren Krewenki <krewenki@gmail.com>

--- jquery.lightbox.js
+++ jquery.lightbox.js
@@ -307,14 +307,16 @@
 
 			nav_html = opts.strings.image + (opts.activeImage + 1) + opts.strings.of + opts.imageArray.length;
 
-			// display previous / next text links
-			if ((opts.activeImage) > 0) {
-				nav_html = '<a title="' + opts.strings.prevLinkTitle + '" href="#" id="prevLinkText">' + opts.strings.prevLinkText + "</a>" + nav_html;
-			}
+			if (!opts.disableNavbarLinks) {
+        // display previous / next text links
+        if ((opts.activeImage) > 0) {
+          nav_html = '<a title="' + opts.strings.prevLinkTitle + '" href="#" id="prevLinkText">' + opts.strings.prevLinkText + "</a>" + nav_html;
+        }
 
-			if ((opts.activeImage + 1) < opts.imageArray.length) {
-				nav_html += '<a title="' + opts.strings.nextLinkTitle + '" href="#" id="nextLinkText">' + opts.strings.nextLinkText + "</a>";
-			}
+        if ((opts.activeImage + 1) < opts.imageArray.length) {
+          nav_html += '<a title="' + opts.strings.nextLinkTitle + '" href="#" id="nextLinkText">' + opts.strings.nextLinkText + "</a>";
+        }
+      }
 
 			$('#numberDisplay').html(nav_html).show();
 		}
@@ -390,6 +392,7 @@
 			image: 'Image ',
 			of: ' of '
 		},
-		fitToScreen: false		// resize images if they are bigger than window
+		fitToScreen: false,		// resize images if they are bigger than window
+    disableNavbarLinks: false
 	};
 })(jQuery);
